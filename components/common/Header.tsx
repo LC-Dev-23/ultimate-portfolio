e56e8b26abd12c970995f21659e0e085444f4f78
LC-Dev <lordchristianagra23@gmail.com>
2024-03-26 22:01:54 +0800

    style(header): adding color to the light mode header.

--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -30,11 +30,11 @@ const Header = () => {
   return (
     <>
 
-      <header className='relative w-full bg-black'>
-        <nav className='flex content-center items-center justify-between py-5 mx-auto px-10 md:px-20 md:min-w-[40rem] max-w-[75rem]'>
-          <Link href={"/"}>
+      <header className='relative w-full bg-white dark:bg-black shadow-2xl'>
+        <nav className='flex content-center items-center justify-between py-5 mx-auto md:min-w-[40rem] max-w-[90%]'>
+          <Link href={"/"} className=''>
             <Image
-              className='w-auto h-16'
+              className='w-auto h-16 invert dark:filter-none'
               src="/images/lc-dev-logo-white.png"
               width={100}
               height={100}
@@ -43,7 +43,7 @@ const Header = () => {
             />
           </Link>
           {/* Desktop Nav */}
-          <ul className={`text-white gap-10 items-center hidden lg:flex`}>
+          <ul className={`text-black dark:text-white gap-10 items-center hidden lg:flex`}>
             {links.map(({ href, text, className }) => (
               <Link key={href} href={href} className={className}>
                 <li className={pathname === href ? 'text-[#DE0000] font-bold scale-110' : 'hover:text-[#DE0000] hover:scale-110'}>
@@ -54,21 +54,13 @@ const Header = () => {
             <ModeToggle />
           </ul>
           <Link className='hidden lg:block' href={"/contact"}>
-            <button className={`${"/contact" === pathname ? "border-[#DE0000]" : "border-white"} border-2 hover:border-[#DE0000] text-white px-5 py-2 `}>CONTACT</button>
+            <button className={` text-black dark:text-white border-2 hover:border-[#DE0000] px-5 py-2 shadow-2xl ${"/contact" === pathname ? "border-[#DE0000]" : "border-black dark:border-white"}`}>CONTACT</button>
           </Link>
-          {isOpen ?
-            (
-              <Button onClick={toggleMenu} variant={'outline'} size={'icon'} className='h-10 w-10 lg:hidden text-white text-4xl bg-transparent'><RiCloseLine />  </Button>
-            )
-            :
-            (
-              <Button onClick={toggleMenu} variant={'outline'} size={'icon'} className='h-10 w-10 lg:hidden text-white text-4xl bg-transparent'><RiMenu2Line /></Button>
-            )}
-
+          <Button onClick={toggleMenu} variant={'outline'} size={'icon'} className='h-10 w-10 lg:hidden text-black dark:text-white text-4xl bg-transparent'>{isOpen ? <RiCloseLine /> : <RiMenu2Line />}</Button>
         </nav>
         {/* Mobile Nav */}
         <nav className={`${isOpen ? "block" : "hidden"} h-screen lg:h-auto`}>
-          <ul className={`text-white gap-10 items-center flex flex-col pt-20 lg:hidden`}>
+          <ul className={`text-black dark:text-white gap-10 items-center flex flex-col pt-20 lg:hidden`}>
             {links.map(({ href, text, className }) => (
               <Link key={href} href={href}>
                 <li className={pathname === href ? 'text-[#DE0000] font-bold scale-110' : 'hover:text-[#DE0000] hover:scale-110'}>
