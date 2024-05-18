47446766ec0aea35908a3c4a35d4872ee7c52c9d
LC-Dev <lordchristianagra23@gmail.com>
2024-05-19 01:10:28 +0800

    style: minor color adjustment

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -1,8 +1,6 @@
 "use client"
- 
+
 import * as React from "react"
-import { SunIcon, MoonIcon } from "lucide-react"
-import { useTheme } from "next-themes"
  
 import { Button } from "@/components/ui/button"
 import {
@@ -11,6 +9,8 @@ import {
   DropdownMenuItem,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu"
+import { useTheme } from "next-themes"
+import { MoonIcon, SunIcon } from "lucide-react"
 
  
 export function ModeToggle() {
@@ -19,20 +19,20 @@ export function ModeToggle() {
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <Button className="text-black dark:text-white" variant="outline" size="icon">
+        <Button className="dark:text-black text-white" variant="link" size="icon">
           <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
           <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
           <span className="sr-only">Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
       <DropdownMenuContent align="end">
-        <DropdownMenuItem onClick={() => { setTheme("light"); console.log("Theme set to light but in reality it is", theme);}}>
+        <DropdownMenuItem className={`${theme== "light" &&  "bg-gray-300"}`} onClick={() => { setTheme("light")}}>
           Light
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => { setTheme("dark"); console.log("Theme set to dark but in reality it is", theme);}}>
+        <DropdownMenuItem onClick={() => { setTheme("dark")}}>
           Dark
         </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => { setTheme("system"); console.log("Theme set to system but in reality it is", theme);}}>
+        <DropdownMenuItem onClick={() => { setTheme("system")}}>
           System
         </DropdownMenuItem>
       </DropdownMenuContent>
--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -28,54 +28,48 @@ const Header = () => {
     { href: '/contact', text: 'CONTACT', className: 'block lg:hidden' },
   ];
   return (
-    <>
-
-      <header className='relative w-full bg-white dark:bg-black shadow-2xl'>
-        <nav className='flex content-center items-center justify-between py-5 mx-auto md:min-w-[40rem] max-w-[90%]'>
-          <Link href={"/"} className=''>
-            <Image
-              className='w-auto h-16 invert dark:filter-none'
-              src="/images/lc-dev-logo-white.png"
-              width={100}
-              height={100}
-              alt="LC Dev Logo"
-              priority={true}
-            />
-          </Link>
-          {/* Desktop Nav */}
-          <ul className={`text-black dark:text-white gap-10 items-center hidden lg:flex`}>
-            {links.map(({ href, text, className }) => (
-              <Link key={href} href={href} className={className}>
-                <li className={pathname === href ? 'text-[#DE0000] font-bold scale-110' : 'hover:text-[#DE0000] hover:scale-110'}>
-                  {text}
-                </li>
-              </Link>
-            ))}
-            <ModeToggle />
-          </ul>
-          <Link className='hidden lg:block' href={"/contact"}>
-            <button className={` text-black dark:text-white border-2 hover:border-[#DE0000] px-5 py-2 shadow-2xl ${"/contact" === pathname ? "border-[#DE0000]" : "border-black dark:border-white"}`}>CONTACT</button>
-          </Link>
-          <Button onClick={toggleMenu} variant={'outline'} size={'icon'} className='h-10 w-10 lg:hidden text-black dark:text-white text-4xl bg-transparent'>{isOpen ? <RiCloseLine /> : <RiMenu2Line />}</Button>
-        </nav>
-        {/* Mobile Nav */}
-        <nav className={`${isOpen ? "block" : "hidden"} h-screen lg:h-auto`}>
-          <ul className={`text-black dark:text-white gap-10 items-center flex flex-col pt-20 lg:hidden`}>
-            {links.map(({ href, text, className }) => (
-              <Link key={href} href={href}>
-                <li className={pathname === href ? 'text-[#DE0000] font-bold scale-110' : 'hover:text-[#DE0000] hover:scale-110'}>
-                  {text}
-                </li>
-              </Link>
-            ))}
-            <ModeToggle />
-          </ul>
-        </nav>
-      </header>
-
-
-    </>
-
+    <header className='z-50 fixed w-full  dark:bg-white/90 bg-black/90 backdrop-blur-sm shadow-sm rounded-b-xl'>
+      <nav className='flex content-center items-center justify-between py-2 container mx-auto'>
+        <Link href={"/"} className=''>
+          <Image
+            className='w-auto h-16 dark:invert filter-none'
+            src="/images/lc-dev-logo-white.png"
+            width={100}
+            height={100}
+            alt="LC Dev Logo"
+            priority={true}
+          />
+        </Link>
+        {/* Desktop Nav */}
+        <ul className={`dark:text-black text-white font-semibold gap-10 items-center hidden lg:flex`}>
+          {links.map(({ href, text, className }) => (
+            <Link key={href} href={href} className={className}>
+              <li className={pathname === href ? 'text-accentRed font-bold scale-110' : 'hover:text-accentRed hover:scale-110'}>
+                {text}
+              </li>
+            </Link>
+          ))}
+          <ModeToggle />
+        </ul>
+        <Link className='hidden lg:block' href={"/contact"}>
+          <button className={` dark:text-black text-white font-semibold border-[3px] dark:hover:border-accentRed hover:border-accentRed px-5 py-2 shadow-2xl ${"/contact" === pathname ? "border-accentRed text-accentRed" : "dark:border-black border-white"}`}>CONTACT</button>
+        </Link>
+        <Button onClick={toggleMenu} variant="link" size={'icon'} className='h-10 w-10 lg:hidden dark:text-black text-white text-4xl bg-transparent'>{isOpen ? <RiCloseLine /> : <RiMenu2Line />}</Button>
+      </nav>
+      {/* Mobile Nav */}
+      <nav className={`${isOpen ? "block" : "hidden"} h-screen lg:h-auto`}>
+        <ul className={`dark:text-black text-white gap-10 items-center flex flex-col pt-20 lg:hidden`}>
+          {links.map(({ href, text, className }) => (
+            <Link key={href} href={href}>
+              <li className={pathname === href ? 'text-accentRed font-bold scale-110' : 'hover:text-accentRed hover:scale-110'}>
+                {text}
+              </li>
+            </Link>
+          ))}
+          <ModeToggle />
+        </ul>
+      </nav>
+    </header>
   )
 }
 
