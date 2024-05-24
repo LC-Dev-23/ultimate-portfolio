75027c6d17472610aac86e8b9970a90feafef20f
Christian Agra <lordchristianagra23@gmail.com>
2024-05-24 10:55:41 +0800

    feat: replacing dropdown theme button with a toggle onclick and make dark mode default

--- components/ModeToggle.tsx
+++ components/ModeToggle.tsx
@@ -1,41 +1,33 @@
 "use client"
 
 import * as React from "react"
- 
+
 import { Button } from "@/components/ui/button"
-import {
-  DropdownMenu,
-  DropdownMenuContent,
-  DropdownMenuItem,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
 import { useTheme } from "next-themes"
 import { MoonIcon, SunIcon } from "lucide-react"
+import { useEffect, useState } from "react"
 
- 
+
 export function ModeToggle() {
-  const { theme ,setTheme } = useTheme()
- 
+  const { theme, setTheme } = useTheme()
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+      setMounted(true)
+  }, [])
+
+  if (!mounted) {
+      return null
+  }
+
+  const toggleDarkMode = () => {
+      setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
+
   return (
-    <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        <Button className="dark:text-black text-white" variant="link" size="icon">
-          <SunIcon className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-          <MoonIcon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-          <span className="sr-only">Toggle theme</span>
-        </Button>
-      </DropdownMenuTrigger>
-      <DropdownMenuContent align="end">
-        <DropdownMenuItem className={`${theme== "light" &&  "bg-gray-300"}`} onClick={() => { setTheme("light")}}>
-          Light
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => { setTheme("dark")}}>
-          Dark
-        </DropdownMenuItem>
-        <DropdownMenuItem onClick={() => { setTheme("system")}}>
-          System
-        </DropdownMenuItem>
-      </DropdownMenuContent>
-    </DropdownMenu>
+    <Button onClick={toggleDarkMode} className="dark:text-black text-white" variant="link" size="icon" aria-label="Click to change theme">
+      {theme === 'dark' ? <SunIcon className="h-[1.2rem] w-[1.2rem]" /> : <MoonIcon className="h-[1.2rem] w-[1.2rem]" />}
+    </Button>
   )
 }
